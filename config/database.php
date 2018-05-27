44386b78f43e0bb7475b8072ea355b24fb6e57d0
Muhammed Essa Hameed <muhammed.essa@gmail.com>
2018-05-28 01:58:56 +0300

    make it better

--- config/database.php
+++ config/database.php
@@ -41,17 +41,17 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' =>  'us-cdbr-iron-east-04.cleardb.net',
-            
-            'database' => 'heroku_7030258f8a25b17',
-            'username' => 'b6264cc658e41c',
-            'password' => '307ffcf3',
-             
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
-             
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
@@ -118,3 +118,28 @@ return [
     ],
 
 ];
+
+
+
+
+
+
+
+
+
+
+
+// 'mysql' => [
+//     'driver' => 'mysql',
+//     'host' =>  'us-cdbr-iron-east-04.cleardb.net',
+    
+//     'database' => 'heroku_7030258f8a25b17',
+//     'username' => 'b6264cc658e41c',
+//     'password' => '307ffcf3',
+     
+//     'charset' => 'utf8mb4',
+//     'collation' => 'utf8mb4_unicode_ci',
+//     'prefix' => '',
+//     'strict' => false,
+     
+// ],
