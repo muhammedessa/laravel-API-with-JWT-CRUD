3415fcbc50af8525c0d7cff10ce411fce7c3907d
Muhammed Essa Hameed <muhammed.essa@gmail.com>
2018-05-28 02:04:57 +0300

    make it better

--- config/database.php
+++ config/database.php
@@ -39,20 +39,20 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+         'mysql' => [
+                'driver' => 'mysql',
+                'host' =>  'us-cdbr-iron-east-04.cleardb.net',
+                
+                'database' => 'heroku_7030258f8a25b17',
+                'username' => 'b6264cc658e41c',
+                'password' => '307ffcf3',
+                
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => false,
+                
+            ],
 
         'pgsql' => [
             'driver' => 'pgsql',
